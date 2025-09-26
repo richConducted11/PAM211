8d541acabeb9bba61b8720a0172cf587ed6f2074
richConducted11 <124049583@upq.edu.mx>
2025-09-25 20:07:38 -0600

    Un pequeño cambio para verificar el funcionamiento

--- Practica 3/main.js
+++ Practica 3/main.js
@@ -3,8 +3,8 @@ import { restar } from './utils.js'
 let resultado1 = restar(10, 4);
 console.log("Prueba 1 (10 - 4):", resultado1); 
 
-let resultado2 = restar(5, -3);
-console.log("Prueba 2 (5 - (-3)):", resultado2); 
+let resultado2 = restar(5, 3);
+console.log("Prueba 2 (5 - 3):", resultado2); 
 
 let resultado3 = restar(2, 8);
 console.log("Prueba 3 (2 - 8):", resultado3); 
