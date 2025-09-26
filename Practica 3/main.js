b1796cb2d9961556f9e0bb779f1e127963bc3790
richConducted11 <124049583@upq.edu.mx>
2025-09-25 20:04:12 -0600

    Primer ejercicio terminado
    Hace las restas correctamente con el import y export para que funcione

---
+++ Practica 3/main.js
@@ -0,0 +1,13 @@
+import { restar } from './utils.js'
+
+let resultado1 = restar(10, 4);
+console.log("Prueba 1 (10 - 4):", resultado1); 
+
+let resultado2 = restar(5, -3);
+console.log("Prueba 2 (5 - (-3)):", resultado2); 
+
+let resultado3 = restar(2, 8);
+console.log("Prueba 3 (2 - 8):", resultado3); 
+
+let resultado4 = restar(15.5, 5.2);
+console.log("Prueba 4 (15.5 - 5.2):", resultado4); 
---
+++ Practica 3/utils.js
@@ -0,0 +1,4 @@
+function restar (a, b){
+    return a - b;
+}
+export { restar };
