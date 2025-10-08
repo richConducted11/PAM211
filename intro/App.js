ee6143bda006eb65fb088ed26bfb9e62eb22cae3
richConducted11 <124049583@upq.edu.mx>
2025-10-08 11:02:52 -0600

    Creación de botones y sus acciones

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+//1. Imports: Zona de imports
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+
+
+//2. Main: Zona de componentes
 export default function App() {
+
+  const [contador, setContador]= useState(0);
+
   return (
+
     <View style={styles.container}>
-      <Text>Hola Mundo React Native</Text>
+
+      <Text> Contador: {contador} </Text>
+      <Button title='Agregar' onPress={()=>setContador(contador+1)} />
+        <Button title='Quitar' onPress={()=>setContador(contador-1)} />
+        <Button title='Reiniciar' onPress={()=>setContador(0)} />
+
       <StatusBar style="auto" />
+
     </View>
+
+
   );
 }
 
+//3. Estilos: Zona de estética para componentes
 const styles = StyleSheet.create({
   container: {
     flex: 1,
