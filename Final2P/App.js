82b04146016d4158af105f4a4ec8510b7bdeda6a
richConducted11 <124049583@upq.edu.mx>
2025-12-08 14:33:45 -0600

    Se creó el archivo blank para el FInal2P
    Se agregaron las dependencias necesarias y unos pequeños cambios para mandar a llamar la screen desde el app.js

---
+++ Final2P/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Final from './Final.js';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Final />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
